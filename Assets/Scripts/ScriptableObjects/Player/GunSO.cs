44dbad0fe2d0278913e46c9f66e21045bdef38b8
Jaume Riera <jaumerg95@gmail.com>
2024-11-15 19:51:16 +0100

    Update gun scriptable object and create new one for eenemies

--- Assets/Scripts/ScriptableObjects/Player/GunSO.cs
+++ Assets/Scripts/ScriptableObjects/Player/GunSO.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GunScriptableObject", menuName = "Scriptable Objects/GunScriptableObject")]
 public class GunSO : ScriptableObject
 {
-    public float aimTime;
+    [Header("Time configurations")]
+    public float shotCooldown;
+    public float timeToAim;
+
+    [Header("Distance configurations")]
+    public float range;
+
+    [Header("Layer collision configurations")]
+    public Layer[] collisionLayers;
+    public int collisionLayerMask;
+
+    [Header("Visual configurations")]
     public Sprite gunSprite;
 }
