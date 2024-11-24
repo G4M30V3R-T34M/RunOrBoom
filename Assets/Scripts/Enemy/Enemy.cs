38958c4871371ac0503a3360034979d965c08491
Jaume Riera <jaumerg95@gmail.com>
2024-11-24 19:09:38 +0100

    Make enemy pooblable

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
+using FeTo.ObjectPool;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : PoolableObject
 {
     [SerializeField] EnemySO enemySettings;
     private HealthManager healthManager;
