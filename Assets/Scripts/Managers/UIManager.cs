f82a6b268fb1b7ac42d4e351e6ab4b6e27b8b188
Jaume Riera <jaumerg95@gmail.com>
2024-11-25 22:34:16 +0100

    Game manager and ui to display how many codes has been picked

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Image[] codes;
+
+    private Queue<Image> codesQueue = new Queue<Image>();
+
+    private void Start() => StartQueue();
+
+    private void StartQueue()
+    {
+        foreach (Image code in codes)
+        {
+            codesQueue.Enqueue(code);
+        }
+    }
+
+    public void PickUpCode()
+    {
+        if (codesQueue.Count == 0)
+        {
+            return;
+        }
+        Image code = codesQueue.Dequeue();
+        Color currentColor = code.color;
+        currentColor.a = 1;
+        code.color = currentColor;
+    }
+}
